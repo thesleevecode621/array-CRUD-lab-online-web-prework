5281f617d12f883c1fcd6342652bc072cf3fc2d4
boruch d <dubywider@gmail.com>
2019-03-31 20:00:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ end
 def add_element_to_end_of_array(array, element)
   array = ["wow", "I", "am", "really", "learning"]
      
-    array.push()
+    array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
